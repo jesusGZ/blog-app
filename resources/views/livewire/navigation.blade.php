4731ee6eb090c44792649c181b48104aeaf74969
jesusGZ <j.gr.zurita@gmail.com>
2021-06-05 10:19:37 -0500

    Correción de del evento:logout

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -79,7 +79,7 @@
               Tu perfil</a>
             <form method="POST" action="{{ route('logout') }}">
               @csrf
-            <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2" onclick="event.eventDefault(); 
+            <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2" onclick="event.preventDefault();
             this.closest('form').submit();">
               {{ __('Cerrar sesión') }}</a>
             </form>
