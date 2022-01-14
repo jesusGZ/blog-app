0314e6824bb198b08b9d430a1f6c03182513b2ea
jesusGZ <j.gr.zurita@gmail.com>
2022-01-14 10:43:52 -0600

    vista para crear categoria

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -7,13 +7,34 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.categories.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre') !!}
+                    {!! Form::text('name', null, ['class' -> 'form-control', 'placeholder' -> 'Ingrese el nombre de la categoría.']) !!}
+                </div>
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+                <div class="form-group">
+                    {!! Form::label('slug', 'Slug') !!}
+                    {!! Form::text('slug', null, ['class' -> 'form-control', 'placeholder' -> 'Ingrese el slug de la categoría.', 'readonly']) !!}
+                </div>
+
+                {!! Form::submit('Crear categoría', ['class' -> 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+    <script>
+        $(document).ready( function () {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            })
+        })
+    </script>
+@endsection
