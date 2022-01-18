9de88465f98ca8d2bf563089e9999623a2aad739
jesusGZ <j.gr.zurita@gmail.com>
2022-01-18 13:01:39 -0600

    cambio

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -99,7 +99,7 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         $category->delete();
-
+        
         return redirect()->route('admin.categories.index')->with('info', 'La categoría se elimino con exito.');
     }
 }
